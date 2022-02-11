36e047e6efbab98f24c4c7d532fc4b28be538450
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-11 10:21:09 +0300

    changing from all to id

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 let box = document.querySelectorAll(".box")
-let btn = document.querySelectorAll("btn")
+let right = document.querySelector("#right")
+let left = document.querySelector("#left")
+let random = document.querySelector("#random")
 let boxes = document.querySelector(".container")
 let counter = 0
+
 box[counter].style.display = "block"
 
 
-// console.log(box[counter])
 
-
-// red.style.display = "block"
 
 right.addEventListener("click", event => {
     box[counter].style.display = "none"
     counter++
+    box[counter].style.display = "block"
+    console.log(counter)
+})
+left.addEventListener("click", event => {
+    box[counter].style.display = "none"
+    counter--
     box[counter].style.display = "block"
     console.log(counter)
 })
