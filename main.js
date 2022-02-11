b0ea3973b957fa15be860847368550d99de639ff
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-11 09:59:50 +0300

    creating toggling with the buttons

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ let box = document.querySelectorAll(".box")
 let right = document.querySelector("#right")
 let boxes = document.querySelector(".container")
 let counter = 0
+box[counter].style.display = "block"
 
-console.log(box[1])
+
+// console.log(box[counter])
 
 
-red.style.display = "block"
+// red.style.display = "block"
 
 right.addEventListener("click", event => {
-    boxes.style.backgroundColor = "blue"
+    box[counter].style.display = "none"
+    counter++
+    box[counter].style.display = "block"
+    console.log(counter)
 })
