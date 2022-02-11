6eae7341f7004cc61e7d028dad4ce6b1c79f498a
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-11 09:52:05 +0300

    Adding buttons

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-let bcolors = ["red", "blue", "yellow", "orange", "green", "indigo"]
 let box = document.querySelectorAll(".box")
+let right = document.querySelector("#right")
+let boxes = document.querySelector(".container")
+let counter = 0
+
+console.log(box[1])
+
+
+red.style.display = "block"
+
+right.addEventListener("click", event => {
+    boxes.style.backgroundColor = "blue"
+})
