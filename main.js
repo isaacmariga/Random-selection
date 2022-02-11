21d8877e0bb11c0fa7c41f28ead96a2a38c4e857
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-11 09:23:54 +0300

    Making the squares hidden

--- main.js
+++ main.js
@@ -1,18 +1,2 @@
 let bcolors = ["red", "blue", "yellow", "orange", "green", "indigo"]
 let box = document.querySelectorAll(".box")
-let red = document.querySelector("#red")
-
-for (i = 0; i < bcolors.length; i++) {
-    bgcolor = (bcolors[i])
-    bcolors = bgcolor.slice(-1)
-
-    console.log(bgcolor)
-}
-
-
-// box.forEach(box => {
-//     box.style.backgroundColor = bcolors
-
-// });
-
-console.log(box[1])
