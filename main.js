5f05f89d894e421df0e8bf9558ebae8150609f4e
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-11 10:37:46 +0300

    Adding functionality to the buttons

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let box = document.querySelectorAll(".box")
 let right = document.querySelector("#right")
 let left = document.querySelector("#left")
 let random = document.querySelector("#random")
+let btn = document.querySelectorAll(".btn")
 let boxes = document.querySelector(".container")
 let counter = 0
+
 
 box[counter].style.display = "block"
 
@@ -13,12 +15,25 @@ box[counter].style.display = "block"
 right.addEventListener("click", event => {
     box[counter].style.display = "none"
     counter++
+    if (counter >= box.length) {
+        counter = 0
+    }
+
     box[counter].style.display = "block"
-    console.log(counter)
+
 })
 left.addEventListener("click", event => {
     box[counter].style.display = "none"
     counter--
+    if (counter < 0) {
+        counter = 5
+    }
     box[counter].style.display = "block"
-    console.log(counter)
 })
+random.addEventListener("click", event => {
+    box[counter].style.display = "none"
+    counter = (Math.floor(Math.random() * box.length))
+    box[counter].style.display = "block"
+})
+
+// console.log)
